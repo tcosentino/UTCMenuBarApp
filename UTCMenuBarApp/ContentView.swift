3a553e2271883e9d44ea7fcda210c75e3283063e
Troy Cosentino <troycosentino@gmail.com>
2024-09-11 13:54:19 -0700

    Initial Commit

---
+++ UTCMenuBarApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UTCMenuBarApp
+//
+//  Created by Troy Cosentino on 9/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UTCMenuBarApp/UTCMenuBarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UTCMenuBarAppApp.swift
+//  UTCMenuBarApp
+//
+//  Created by Troy Cosentino on 9/11/24.
+//
+
+import SwiftUI
+
+@main
+struct UTCMenuBarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
